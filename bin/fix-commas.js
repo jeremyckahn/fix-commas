31c098ce0b4edd1810ac56d7f50b9599a96dd41a
Jeremy Kahn <jeremy@jellyvision.com>
2017-06-29 11:39:14 -0500

    Add --fix option

--- bin/fix-commas.js
+++ bin/fix-commas.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env node
 
 const commander = require('commander');
-const { readFileSync }  = require('fs');
+const { readFileSync, writeFileSync }  = require('fs');
 const fixCommas = require('../dist/index').default;
 
 const { version } = require('../package.json');
 
 commander
   .version(version)
+  .option('--fix', 'Overwrite file with fixed code')
   .parse(process.argv);
 
 const { args } = commander;
 
-args.forEach(file =>
-  process.stdout.write(
-    fixCommas(readFileSync(file, 'utf8'))
-  )
-);
+args.forEach(file => {
+  const fixedCode = fixCommas(readFileSync(file, 'utf8'));
+
+  if (commander.fix) {
+    writeFileSync(file, fixedCode, 'utf8')
+  } else {
+    process.stdout.write(fixedCode)
+  }
+});
